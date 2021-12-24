c90ac40d881efaf46e821c7b88d12d46fd39622d
Gianni Carafa <gianni.carafa@srf.ch>
2021-12-24 22:54:58 +0100

    load cli vars to App

--- pkg/klustair/main.go
+++ pkg/klustair/main.go
@@ -6,8 +6,44 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+type Options struct {
+	Namespaces           string
+	NamespacesBlacklist  string
+	KubeAudit            string
+	Trivy                bool
+	Label                string
+	TrivyCredentialsPath string
+	LimitDate            int
+	LimitNr              int
+	configkey            string
+}
+
 func RunCli(ctx *cli.Context) error {
 	fmt.Println("run")
+	opt, _ := loadOpts(ctx)
+	Run(opt)
 	return nil
 	//return xerrors.Errorf("option error: %w", "nothing to do")
+}
+
+func Run(opt Options) error {
+	fmt.Printf("run with options: %+v\n", opt)
+
+	return nil
+	//return xerrors.Errorf("option error: %w", "nothing to do")
+}
+
+func loadOpts(ctx *cli.Context) (Options, error) {
+	opt := Options{
+		Namespaces:           ctx.String("namespaces"),
+		NamespacesBlacklist:  ctx.String("namespacesblacklist"),
+		KubeAudit:            ctx.String("kubeaudit"),
+		Trivy:                ctx.Bool("trivy"),
+		Label:                ctx.String("label"),
+		TrivyCredentialsPath: ctx.String("repocredentialspath"),
+		LimitDate:            ctx.Int("limitdate"),
+		LimitNr:              ctx.Int("limitnr"),
+		configkey:            ctx.String("configkey"),
+	}
+	return opt, nil
 }
